256460b681ea8374f88a10403e8b7818920ba762
Ali Mostafa <ali.mostafa.kassab2022@gmail.com>
2025-12-16 02:06:12 +0200

    Fixs

---
+++ CleanArchitecture/CleanArchitecture/CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureApp.swift
+//  CleanArchitecture
+//
+//  Created by Ali Mostafa on 16/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanArchitecture/CleanArchitectureTests/CleanArchitectureTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureTests.swift
+//  CleanArchitectureTests
+//
+//  Created by Ali Mostafa on 16/12/2025.
+//
+
+import Testing
+@testable import CleanArchitecture
+
+struct CleanArchitectureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
